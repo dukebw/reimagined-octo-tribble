ef39896f08665ebd240d75e3c43ffff9de73e392
Brendan Duke <dukebw@mcmaster.ca>
2017-10-30 22:13:59 -0400

    Add initial boiler plate for rot_math.

---
+++ include/rot_math.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2017 Brendan Duke.
+ *
+ * This file is part of ROT ML Library.
+ *
+ * ROT ML Library is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * ROT ML Library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ROT ML Library. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef ROT_MATH_H
+#define ROT_MATH_H
+
+typedef struct rot_tensor *rot_tensor_t;
+
+/**
+ * ROT_matmul()
+ *
+ * Requirements
+ *
+ * Inputs:
+ * Two tensors of dimension == 2.
+ * The inner dimension (number of columns) of the first argument must match the
+ * outer dimension (number of rows) of the second argument.
+ *
+ * Output:
+ * For input tensors of dimension mxn and m'xn', the output tensor is a single
+ * tensor of mxn'.
+ */
+rot_tensor_t ROT_matmul(rot_tensor_t a, rot_tensor_t b);
+
+#endif /* ROT_MATH_H */
