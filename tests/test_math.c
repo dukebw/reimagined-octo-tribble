9c822a2a9fd1ba2c92419ceb12516624baa0ca43
Brendan Duke <dukebw@mcmaster.ca>
2017-10-30 22:14:22 -0400

    Add boiler plate for test_math.

---
+++ tests/test_math.c
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2017 Brendan Duke.
+ *
+ * This file is part of ROT ML Library.
+ *
+ * ROT ML Library is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * ROT ML Library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ROT ML Library. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "rot_math.h"
+#include "tests/min_unit.h"
+
+#include <stdlib.h>
+
+static MIN_UNIT_TEST_FUNC(test_matmul)
+{
+        rot_tensor_t c = ROT_matmul(NULL, NULL);
+}
+
+int main(void)
+{
+        test_matmul();
+
+        printf("All tests passed!\n");
+
+        return EXIT_SUCCESS;
+}
